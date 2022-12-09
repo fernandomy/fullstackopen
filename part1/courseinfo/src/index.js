974d8d7264cb6ed479e3a17b068a4a64c3103ed2
Fernando M. Yncaluque <fyncaluque@gmail.com>
2022-12-09 09:55:37 -0500

    Resolved javascripts exercises

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,48 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Header=({course})=><h1>{course}</h1>
+const Header = ({course}) => <h1>{course}</h1>;
 
-const Part =({part})=> <p>{part.title} {part.exercises}</p>
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
 
-const Content=({parts})=>{
- return (
-  <div>
-    {parts.map((part,i)=><Part part={part} key={i}/>)}
-  </div>
- )
-}
-
-const Total = ({parts})=>{
-  const sum= parts.reduce((a,b)=>a+b.exercises,0)
-  return <p>Number of exercises {sum}</p>
-}
-
-const App = () => {
-  const data={
-    course:"Half Stack application development",
-    parts:[
-      {
-        title:"Fundamentals of React",
-        exercises: 10
-      },
-      {
-        title:"Using props to pass data",
-        exercises: 7
-      },
-      {
-        title:"State of a component",
-        exercises: 14
-      }
-    ]
-  } 
+const Content = ({ parts }) => {
   return (
     <div>
-      <Header course={data.course}/>
-      <Content parts={data.parts}/>
-      <Total parts={data.parts}/>
+      {parts.map((part, i) => (
+        <Part part={part} key={i} />
+      ))}
     </div>
   );
 };
 
-ReactDOM.createRoot( document.getElementById("root")).render(<App />);
+const Total = ({ parts }) => {
+  const sum = parts.reduce((a, b) => a + b.exercises, 0);
+  return <p>Number of exercises {sum}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
