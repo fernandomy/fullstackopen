216a7756688da0179ad4910a34c71930467957b2
Fernando M. Yncaluque <fyncaluque@gmail.com>
2022-12-15 16:01:27 -0500

    Part2 Phonebook exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 const Filter = ({ handleFilter, newFilter }) => {
   return (
     <div>
@@ -46,16 +47,16 @@ const Person = ({person}) => {
 )
 }
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setFilter] = useState("");
 
+  useEffect(()=>{
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response=>setPersons(response.data))
+  },[])
   const handleName = (e) => {
     setNewName(e.target.value);
   };
